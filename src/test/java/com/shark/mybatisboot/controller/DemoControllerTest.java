c95f9a4bbfd9fca42542e6faac373fbacd615307
shark <sharkshore@163.com>
2017-12-04 09:33:54 +0800

    add jsonpath test class

---
+++ src/test/java/com/shark/mybatisboot/controller/DemoControllerTest.java
@@ -0,0 +1,54 @@
+package com.shark.mybatisboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tuze on 2017/12/1.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class DemoControllerTest {
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void hello() throws Exception {
+
+    }
+
+    @Test
+    public void demo() throws Exception {
+
+    }
+
+    @Test
+    public void insert() throws Exception {
+
+    }
+
+    @Test
+    public void testparam() throws Exception {
+
+    }
+
+    @Test
+    public void queryMemberName() throws Exception {
+
+    }
+
+    @Test
+    public void querySqlModel() throws Exception {
+
+    }
+
+}
